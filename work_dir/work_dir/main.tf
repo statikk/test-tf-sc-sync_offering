1b834c98d75c6a3b24e897bd5c341b5e17b12fa5
statikk <greedmysin@gmail.com>
2019-08-07 16:58:56 +0300

    Update main.tf

--- work_dir/work_dir/main.tf
+++ work_dir/work_dir/main.tf
@@ -24,7 +24,7 @@ resource "random_id" "instance_id" {
 }
 
 // A single Google Cloud Engine instance
-resource "google_compute_instance" "default" {
+resource "gooooogle_compute_instance" "default" {
  name         = "flask-vm-${random_id.instance_id.hex}"
  machine_type = "f1-micro"
  zone         = "us-west1-a"
