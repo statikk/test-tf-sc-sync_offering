e0ce1e0f609021f71019243fab3ef24bfa9341ee
statikk <greedmysin@gmail.com>
2019-08-08 16:58:14 +0300

    Update variables.tf

--- work_dir/variables.tf
+++ work_dir/variables.tf
@@ -1,4 +1,13 @@
-variable "var1" {
-    type = "string"
-    default = "test1"
+variable "name" {
+  description = "The machine name"
+}
+
+variable "machine_type" {
+  description = "The machine type to deploy"
+  default = "n1-standard-1"
+}
+
+variable "zone" {
+  description = "The availability zone to use"
+  default = "europe-west1-b"
 }
