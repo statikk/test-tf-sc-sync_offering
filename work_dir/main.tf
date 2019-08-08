78690dd3877906cd406d6ae0faac362749fac573
statikk <greedmysin@gmail.com>
2019-08-08 16:57:38 +0300

    Update main.tf

--- work_dir/main.tf
+++ work_dir/main.tf
@@ -1,51 +1,23 @@
-terraform {
-  backend "remote" {
-    hostname = "terraform.com:8443"
-    organization = "my-organization-0"
-    token = "123"
-
-    workspaces {
-      name = "my-workspace-0"
+resource "google_compute_instance" "my-vm" {
+  name = "${var.name}"
+  machine_type = "${var.machine_type}"
+  zone = "${var.zone}"
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1804-bionic-v20180613"
     }
+  }
+
+  network_interface {
+    network = "default"
+  }
+
+  labels = {
+    tier = "front"
+    stage = "production"
   }
 }
 
-
-// Configure the Google Cloud provider
-provider "google" {
- credentials = "${file("flask-app-876d96137760.json")}"
- project     = "flask-app-244915"
- region      = "europe-west2"
+output "instance_id" {
+  value = "${google_compute_instance.my-vm.instance_id}"
 }
-
-// Terraform plugin for creating random ids
-resource "random_id" "instance_id" {
- byte_length = 8
-}
-
-// A single Google Cloud Engine instance
-resource "google_compute_instance" "default" {
- name         = "flask-vm-${random_id.instance_id.hex}"
- machine_type = "f1-micro"
- zone         = "us-west1-a"
-
- boot_disk {
-   initialize_params {
-     image = "debian-cloud/debian-9"
-   }
- }
-
-// Make sure flask is installed on all new instances for later steps
- metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential python-pip rsync; pip install flask"
-
- network_interface {
-   network = "default"
-
-   access_config {
-     // Include this section to give the VM an external ip address
-   }
- }
-}
-
-
-
